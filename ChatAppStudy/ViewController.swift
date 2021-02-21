036de865b66273c4c0087f9b55feb2ffe2cb9d7b
Toya Kakohara <toyack1128@gmail.com>
2021-02-21 15:45:45 +0900

    first commit

---
+++ ChatAppStudy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChatAppStudy
+//
+//  Created by 加古原　冬弥 on 2021/02/21.
+//  Copyright © 2021 加古原　冬弥. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
